7e9361a1a012e4a16e17bb21b06942b3c4d53192
Jean Azevedo <jean.azevedo1533@gmail.com>
2024-09-25 21:15:07 -0300

    adicionando PhoneItem e footer no layout

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,83 +1,92 @@
+import PhoneItem from "@/app/components/phone-items";
 import ServiceItem from "@/app/components/service-item";
 import { Button } from "@/app/components/ui/button";
 import { db } from "@/app/lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, Phone, SmartphoneIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-interface BarbershopPageProps{
-    params:{
-        id:string
+interface BarbershopPageProps {
+    params: {
+        id: string
     }
 }
 
-const  BarbershopPage = async ({params}:BarbershopPageProps) => {
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     const barbershop = await db.barbershop.findUnique({
-        where:{
-            id:params.id,
-    },
-        include:{
-            services:true,
+        where: {
+            id: params.id,
+        },
+        include: {
+            services: true,
         }
-        
-    
+
+
     })
-    
-    if(!barbershop){
+
+    if (!barbershop) {
         return notFound()
     }
 
-    
+
 
     return <div>
-        
-            <div className="relative w-full h-[250px]">
-                <Image 
-                src={barbershop?.imageUrl}  
-                alt={barbershop?.name} fill className="object-cover"/>
-                
-                <Button size="icon" variant="secondary" className="absolute top-4 left-4" asChild>
-                    <Link href="/">
-                    <ChevronLeftIcon/>
-                    </Link>
-                </Button>
 
-                <Button size="icon" variant="secondary" className="absolute top-4 right-4">
-                    <MenuIcon/> 
-                </Button>
-                
-                </div>
+        <div className="relative w-full h-[250px]">
+            <Image
+                src={barbershop?.imageUrl}
+                alt={barbershop?.name} fill className="object-cover" />
 
-                <div className="p-5 border-b border-solid">
-                <h1 className="text-xl font-bold mb-3">{barbershop?.name}</h1>
-                    <div className="flex items-center gap-2 mb-2 ">
-                        <MapPinIcon className="text-primary" size={18}/>
-                        <p className="text-sm">{barbershop?.address}</p>
-                    </div>
+            <Button size="icon" variant="secondary" className="absolute top-4 left-4" asChild>
+                <Link href="/">
+                    <ChevronLeftIcon />
+                </Link>
+            </Button>
 
-                    <div className="flex items-center gap-2 ">
-                        <StarIcon className="text-primary fill-primary"size={18}/>
-                        <p className="text-sm">4,8 (499 avaliações)</p>
-                    </div>  
-                </div>
+            <Button size="icon" variant="secondary" className="absolute top-4 right-4">
+                <MenuIcon />
+            </Button>
 
-                <div className="p-5 border-b border-solid space-y-3">
-                    <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
-                    <p className="text-sm text-justify">{barbershop?.description}</p>
-                </div>
+        </div>
 
-                <div className="p-5 space-y-3" >
-                    <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
-                <div className="space-y-3">
-                {barbershop.services.map(service=><ServiceItem key={service.id} service={service}/>)}
-                    </div>
-                </div>
-                
+        <div className="p-5 border-b border-solid">
+            <h1 className="text-xl font-bold mb-3">{barbershop?.name}</h1>
+            <div className="flex items-center gap-2 mb-2 ">
+                <MapPinIcon className="text-primary" size={18} />
+                <p className="text-sm">{barbershop?.address}</p>
             </div>
-        
-        
-     
+
+            <div className="flex items-center gap-2 ">
+                <StarIcon className="text-primary fill-primary" size={18} />
+                <p className="text-sm">4,8 (499 avaliações)</p>
+            </div>
+        </div>
+        {/*DESCRIÇÃO*/}
+        <div className="p-5 border-b border-solid space-y-3">
+            <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+            <p className="text-sm text-justify">{barbershop?.description}</p>
+        </div>
+        {/*SERVIÇOS*/}
+        <div className="p-5 space-y-3 border-b border-solid" >
+            <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+            <div className="space-y-3">
+                {barbershop.services.map(service => <ServiceItem key={service.id} service={service} />)}
+            </div>
+        </div>
+
+        {/*CONTATO*/}
+
+        <div className="p-5 space-y-3">
+            {barbershop.phones.map((phone) => (
+                <PhoneItem key={phone} phone={phone} />
+
+            ))}
+        </div>
+    </div>
+
+
+
 }
- 
-export default BarbershopPage ;
+
+export default BarbershopPage;
--- app/page.tsx
+++ app/page.tsx
@@ -12,79 +12,73 @@ import BookingItem from './components/booking-item';
 
 
 
-const Home = async () =>{ 
+const Home = async () => {
   //chamar banco de dados
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
-    name: "desc",
+      name: "desc",
     },
   })
-  
- 
-  return ( 
-     <div>
-     <Header/>
-     <div className="p-5">
-     <h2 className="text-xl font-bold">Olá, Jean</h2>
-     <p>Quarta-feira,07 de agosto</p>
-     
-
-     <div className="flex items-center gap-2 mt-6">
-     <Input placeholder='Faça sua busca...'></Input>
-     <Button>
-      <SearchIcon/>
-     </Button>
-     </div>
 
 
-     {/*BUSCA RAPIDA*/}
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jean</h2>
+        <p>Quarta-feira,07 de agosto</p>
 
-     <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-      {QuickSearchOptions.map((option)=>(
 
-       <Button className='gap-2' variant="secondary">
-       <Image src={option.imageUrl} width={16} height={16} alt={option.title}></Image>
-       {option.title}
-     </Button>
-      
-      ))}
-     </div>
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder='Faça sua busca...'></Input>
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
 
-     <div className='relative w-full h-[150px] mt-6 >'>
-     <Image src="/banner-01.png" fill className="object-cover rounded-xl" alt='Banner'/>
-     </div>
 
-    <BookingItem/>
+        {/*BUSCA RAPIDA*/}
+
+        <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {QuickSearchOptions.map((option) => (
+
+            <Button className='gap-2' variant="secondary">
+              <Image src={option.imageUrl} width={16} height={16} alt={option.title}></Image>
+              {option.title}
+            </Button>
+
+          ))}
+        </div>
+
+        <div className='relative w-full h-[150px] mt-6 >'>
+          <Image src="/banner-01.png" fill className="object-cover rounded-xl" alt='Banner' />
+        </div>
+
+        <BookingItem />
 
         <h2 className='text-xs font-bold uppercase text-gray-400 mt-6 mb-3'>Recomendados</h2>
 
         <div className='flex gap-4 overflow-auto  [&::-webkit-scrollbar]:hidden'>
-        {barbershops.map((barbershop)=> (
-          <BarbershopItem  key={barbershop.id} barbershop={barbershop}/>
-        ))}
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
 
-         <h2 className='text-xs font-bold uppercase text-gray-400 mt-6 mb-3'>Populares</h2>
+        <h2 className='text-xs font-bold uppercase text-gray-400 mt-6 mb-3'>Populares</h2>
 
-      <div className='flex gap-4 overflow-auto  [&::-webkit-scrollbar]:hidden'>
-      {popularBarbershops.map((barbershop)=> (
-        <BarbershopItem  key={barbershop.id} barbershop={barbershop}/>
-      ))}
+        <div className='flex gap-4 overflow-auto  [&::-webkit-scrollbar]:hidden'>
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
       </div>
-     
-        </div>
-      <footer>
-      <Card>
-        <CardContent className='px-5 py-6'>
-        <p className='text-sm text-gray-400'>2024 Copyrigth <span className='font-bold'>Fsw Barber</span></p>
-          </CardContent>
-          </Card>     
-      </footer>
-        </div>
+
+    </div>
   )
-    
-  
+
+
 };
 
 export default Home;
