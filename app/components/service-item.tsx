eed060f31bfc229cb5808b03b187aff353f6c1bb
Jean Azevedo <jean.azevedo1533@gmail.com>
2024-11-04 23:21:50 -0300

    feat:add calendar

--- app/components/service-item.tsx
+++ app/components/service-item.tsx
@@ -1,7 +1,12 @@
+"use client"
 import { BarbershopService } from "@prisma/client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Calendar } from "./ui/calendar";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 interface ServiceItemProps{
     service:BarbershopService
@@ -13,7 +18,7 @@ const ServiceItem = ({service}:ServiceItemProps) => {
   <Card>
     <CardContent className="flex items-center gap-3 space-y-3 mt-1">
     
-            <div className="relative max-h-[110px] min-h-[110px] max-w-[110px] min-w-[110px] :">
+            <div className="relative max-h-[110px] min-h-[110px] max-w-[110px] min-w-[110px]">
             <Image src={service.imageUrl} alt={service.name} fill className="object-cover rounded-xl"/>
             </div>
             <div className="space-y-2">
@@ -27,8 +32,47 @@ const ServiceItem = ({service}:ServiceItemProps) => {
                         currency:"BRL"
                     }).format(Number(service.price))}</p>
 
-                    <Button variant="secondary" size="sm">Reservar</Button>
-
+                    <Sheet>
+                        <SheetTrigger>
+                        <Button variant="secondary" size="sm">Reservar</Button>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                <SheetTitle>Fazer Reserva</SheetTitle>
+                            </SheetHeader>
+                            <div className="py-5"> 
+                                <Calendar 
+                                mode="single" 
+                                locale={ptBR}
+                                styles={{
+                                    head_cell: {
+                                      width: "100%",
+                                      textTransform: "capitalize",
+                                    },
+                                    cell: {
+                                      width: "100%",
+                                    },
+                                    button: {
+                                      width: "100%",
+                                    },
+                                    nav_button_previous: {
+                                      width: "32px",
+                                      height: "32px",
+                                    },
+                                    nav_button_next: {
+                                      width: "32px",
+                                      height: "32px",
+                                    },
+                                    caption: {
+                                      textTransform: "capitalize",
+                                    },
+                                }}
+                                ></Calendar>
+                            </div>
+                        
+                        </SheetContent>
+                    </Sheet>
+                    
                     </div>
             </div>
         
